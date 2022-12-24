9ef1fa89562f75c2506f8feac6dd4f9dd6152228
虫 <tokinobug@163.com>
2022-12-24 20:07:42 +0800

    compute many simulations

--- libthreebodyfractal/threebodyfractal.cpp
+++ libthreebodyfractal/threebodyfractal.cpp
@@ -1,1 +1,18 @@
 #include "threebodyfractal.h"
+
+void libthreebody::compute_many(const input_t *const src, result_t *const dest,
+                                const uint64_t count,
+                                const compute_options &opt) {
+  using namespace libthreebody;
+
+  constexpr int batch_size = 1024;
+
+#pragma omp parallel for schedule(dynamic)
+  for (int batch_idx = 0; batch_idx < (int)std::ceil(float(count) / batch_size);
+       batch_idx++) {
+    for (int idx = batch_idx * batch_size;
+         idx < std::max((batch_idx + 1) * batch_size, int(count)); idx++) {
+      simulate(src[idx], opt, dest + idx);
+    }
+  }
+}
--- libthreebodyfractal/threebodyfractal.h
+++ libthreebodyfractal/threebodyfractal.h
@@ -1,6 +1,12 @@
 #ifndef THREEBODYFRACTAL3_THREEBODYFRACTAL_H
 #define THREEBODYFRACTAL3_THREEBODYFRACTAL_H
 
-#include <fractal_utils/core_utils.h>
+#include "libthreebody.h"
+
+namespace libthreebody {
+
+void compute_many(const input_t *const src, result_t *const dest,
+                  const uint64_t count, const compute_options &opt);
+}
 
 #endif // THREEBODYFRACTAL3_THREEBODYFRACTAL_H
