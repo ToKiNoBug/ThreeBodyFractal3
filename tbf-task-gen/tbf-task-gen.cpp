4e45d1596d9df264f6f7fc35130aeabef3d549c0
ToKiNoBug <tokinobug@163.com>
2023-01-11 12:55:58 +0800

    add tbf-task-gen

---
+++ tbf-task-gen/tbf-task-gen.cpp
@@ -0,0 +1,29 @@
+#include <CLI11.hpp>
+#include <nlohmann/json.hpp>
+#include <string>
+
+struct task_input {
+  size_t rows;
+  size_t cols;
+
+  double y_span;
+
+  std::string center_hex;
+};
+
+int main(int argc, char** argv) {
+  CLI::App app;
+  task_input ti;
+  app.add_option("--rows", ti.rows, "Fractal rows.")
+      ->default_val(320)
+      ->check(CLI::PositiveNumber);
+  app.add_option("--cols", ti.cols, "Fractal cols.")
+      ->default_val(320)
+      ->check(CLI::PositiveNumber);
+  app.add_option("--center-hex", ti.center_hex, "32 digit hex value.")
+      ->default_val("0x00000000000000000000000000000000");
+
+  CLI11_PARSE(app, argc, argv);
+
+  return 0;
+}
